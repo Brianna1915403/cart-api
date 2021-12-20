d39bddcd89f11d53e0960e1d568e2c2eeadcdf63
lawrence122 <54451749+lawrence122@users.noreply.github.com>
2021-12-20 16:42:15 -0500

    Fixed your login

--- client/curlLogin.php
+++ client/curlLogin.php
@@ -6,7 +6,8 @@
 // $2y$10$m6l3k2b4uyumeJQeYCSeXusTFneWCmYtBrlg2I2Hj5KAAaAxAwNYq
     if(isset($_POST['login'])) {
         $result = HTTPLogin();
-        if((!is_null($result)) && password_verify('aaa', $result['password_hash'])){
+
+        if((!is_null($result)) && password_verify($_POST['password'], $result['password_hash'])){
             $_SESSION['NAME'] = $result['clientName'];
             header("location: ".BASE."/curlConvert.php");
         }else{
@@ -52,39 +53,19 @@
         echo "<br />";
 
         return $result;
-
-        // $ch = curl_init();
-        // curl_setopt($ch, CURLOPT_URL, "http://localhost/WebServicesProject/Converter/api/client/898699643");
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json', 'Content-Type:application/json'));
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // $response = curl_exec($ch);
-        // curl_close($ch);
-        // $password = "a";
-        // $response = json_decode($response, true);
-
-        // var_dump($response['password_hash']);
-        // echo "<br>";
-        // var_dump($response[2]);
-        // echo "<br>";
-
-        // echo "Password hash: ";
-        // var_dump(password_verify($password, $response['password_hash']));
-        // echo "<br>Index 2:";
-        // var_dump(password_verify($password, $response[2]));
-        // echo "<br>";
     }
 
     function HTTPRegister(){
         $url = "http://localhost/WebServicesProject/Converter/api/client/create"; // url to create a user.
         
-        $hash_password = password_hash($_POST['password_hash'], PASSWORD_DEFAULT);
+        $hash_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         
         $post = array(
             "clientName" => $_POST['username'],
             "password_hash" => $hash_password
         );
 
-        $_SESSION['PASSWORD'] = $_POST['password_hash'];
+        $_SESSION['PASSWORD'] = $_POST['password'];
 
         $data = json_encode($post);
 
